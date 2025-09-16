84bcd720370d8fbdc46ae95cb93deefd76c3fc21
kitae13 <bkt0403@naver.com>
2025-09-16 10:35:05 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,23 @@
+int ledPin = 7;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+
+  digitalWrite(ledPin, LOW);  
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
+
+  digitalWrite(ledPin, HIGH);  
+
+  while (1) {
+  }
+}
+
+void loop() {
+}
